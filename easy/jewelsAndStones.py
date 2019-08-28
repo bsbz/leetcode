243f78fce4e9131c4f39d82c4e273e5b73827a3e
zolboo <zolboo888@gmail.com>
2019-08-28 09:10:19 -0600

    first easy problem

---
+++ easy/jewelsAndStones.py
@@ -0,0 +1,19 @@
+import math
+
+def numJewelsInStones(J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        setJ = set(S)
+        for jewel in S:
+            if jewel in J:
+                count += 1
+        return count
+
+J = "aA"
+S = "aAAbbbb"
+res = numJewelsInStones(J, S)
+print(res)
