8e28c274f0e5ec8aa16f953367d93cdcce2e5f39
bsbz <zolboo888@gmail.com>
2019-09-05 23:33:30 -0600

    more

--- easy/jewelsAndStones.py
+++ easy/jewelsAndStones.py
@@ -1,3 +1,11 @@
+'''
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+so "a" is considered a different type of stone from "A".
+'''
+
 def numJewelsInStones(J, S):
         """
         :type J: str
@@ -5,7 +13,6 @@ def numJewelsInStones(J, S):
         :rtype: int
         """
         count = 0
-        setJ = set(S)
         for jewel in S:
             if jewel in J:
                 count += 1
---
+++ easy/squaresOfSortedArray.py
@@ -0,0 +1,30 @@
+'''
+Given an array of integers A sorted in non-decreasing order, return an array 
+of the squares of each number, also in sorted non-decreasing order.
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+'''
+
+def sortedSquares(A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        """
+        squares = []
+        for item in A:
+            squares.append(item*item)
+        return sorted(squares)
+        """
+        
+        for i in range(len(A)):
+            A[i] **= 2
+        return sorted(A)
+
+arr = [-4,-1,0,3,10]
+
+res = sortedSquares(arr)
+print(res)
