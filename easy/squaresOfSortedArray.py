534c7948fe7f51c067be343fccbd5086801c0328
bsbz <34457508+bsbz@users.noreply.github.com>
2020-06-11 11:59:52 -0700

    977. Squares of a Sorted Array (array)

--- easy/squaresOfSortedArray.py
+++ easy/squaresOfSortedArray.py
@@ -1,11 +1,11 @@
 '''
-Given an array of integers A sorted in non-decreasing order, return an array 
-of the squares of each number, also in sorted non-decreasing order.
+        Given an array of integers A sorted in non-decreasing order, return an array 
+        of the squares of each number, also in sorted non-decreasing order.
 
-Example 1:
+        Example 1:
 
-Input: [-4,-1,0,3,10]
-Output: [0,1,9,16,100]
+        Input: [-4,-1,0,3,10]
+        Output: [0,1,9,16,100]
 '''
 
 def sortedSquares(A):
